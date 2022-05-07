2e07d54a46ccb2b29b8c9a3988cc1ece6c6b471c
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-07 11:44:24 -0500

    🚧 [ADD] Image Shadow

--- lib/src/widgets/shoes_size_preview/shoes_size.dart
+++ lib/src/widgets/shoes_size_preview/shoes_size.dart
@@ -28,8 +28,27 @@ class _ShoeImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: const [
+        Positioned(bottom: 20, right: 0, child: _ShoeShadow()),
         Image(image: AssetImage("assets/images/azul.png")),
       ],
+    );
+  }
+}
+
+class _ShoeShadow extends StatelessWidget {
+  const _ShoeShadow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        width: 230,
+        height: 120,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: const [BoxShadow(color: Color(0xffEAA14E), blurRadius: 40)]),
+      ),
     );
   }
 }
